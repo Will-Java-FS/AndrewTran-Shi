c1e891e19622a2700760637b0eaa7d47799ff8f3
Andrew T <me@andrewtran-shi.dev>
2024-08-06 11:56:38 -0700

    modified:   .gitignore 	deleted:    Projects/.DS_Store 	deleted:    Projects/.idea/.gitignore 	deleted:    Projects/.idea/Project0-Item-Management-CRUD.iml 	deleted:    Projects/.idea/compiler.xml 	deleted:    Projects/.idea/encodings.xml 	deleted:    Projects/.idea/jarRepositories.xml 	deleted:    Projects/.idea/jpa-buddy-datasource.xml 	deleted:    Projects/.idea/jpa-buddy.xml 	deleted:    Projects/.idea/misc.xml 	deleted:    Projects/.idea/modules.xml 	deleted:    Projects/.idea/vcs.xml 	deleted:    Projects/.vscode/settings.json 	deleted:    Projects/Project0/.gitignore 	deleted:    Projects/Project0/.idea/.gitignore 	deleted:    Projects/Project0/.idea/compiler.xml 	deleted:    Projects/Project0/.idea/encodings.xml 	deleted:    Projects/Project0/.idea/inspectionProfiles/Project_Default.xml 	deleted:    Projects/Project0/.idea/jarRepositories.xml 	deleted:    Projects/Project0/.idea/jpa-buddy.xml 	deleted:    Projects/Project0/.idea/misc.xml 	deleted:    Projects/Project0/.idea/uiDesigner.xml 	deleted:    Projects/Project0/.idea/vcs.xml 	deleted:    Projects/Project0/.mvn/wrapper/maven-wrapper.properties 	deleted:    Projects/Project0/Project0.postman_collection.json 	deleted:    Projects/Project0/mvnw 	deleted:    Projects/Project0/mvnw.cmd 	deleted:    Projects/Project0/pom.xml 	deleted:    Projects/Project0/readme.md 	deleted:    Projects/Project0/sql_ref.md 	deleted:    Projects/Project0/src/main/java/com/revature/Project0/Project0Application.java 	deleted:    Projects/Project0/src/main/java/com/revature/Project0/controllers/ItemController.java 	deleted:    Projects/Project0/src/main/java/com/revature/Project0/controllers/UserController.java 	deleted:    Projects/Project0/src/main/java/com/revature/Project0/models/Item.java 	deleted:    Projects/Project0/src/main/java/com/revature/Project0/models/User.java 	deleted:    Projects/Project0/src/main/java/com/revature/Project0/repositories/ItemRepo.java 	deleted:    Projects/Project0/src/main/java/com/revature/Project0/repositories/UserRepo.java 	deleted:    Projects/Project0/src/main/java/com/revature/Project0/services/ItemService.java 	deleted:    Projects/Project0/src/main/java/com/revature/Project0/services/ItemServiceImpl.java 	deleted:    Projects/Project0/src/main/java/com/revature/Project0/services/UserService.java 	deleted:    Projects/Project0/src/main/java/com/revature/Project0/services/UserServiceImpl.java 	deleted:    Projects/Project0/src/main/resources/application.properties 	deleted:    Projects/Project0/src/test/java/com/revature/Project0/Project0ApplicationTests.java 	deleted:    Projects/Project0/src/test/java/com/revature/Project0/services/ItemServiceImplTest.java 	deleted:    Projects/Project0/src/test/java/com/revature/Project0/services/UserServiceImplTest.java 	deleted:    Projects/Project0/target/Project0-0.0.1-SNAPSHOT.jar.original 	deleted:    Projects/Project0/target/classes/application.properties 	deleted:    Projects/Project0/target/maven-archiver/pom.properties 	deleted:    Projects/Project0/target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst 	deleted:    Projects/Project0/target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst 	deleted:    Projects/Project0/target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/createdFiles.lst 	deleted:    Projects/Project0/target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/inputFiles.lst 	deleted:    Projects/Project0/target/surefire-reports/TEST-com.revature.Project0.services.ItemServiceImplTest.xml 	deleted:    Projects/Project0/target/surefire-reports/TEST-com.revature.Project0.services.UserServiceImplTest.xml 	deleted:    Projects/Project0/target/surefire-reports/com.revature.Project0.services.ItemServiceImplTest.txt 	deleted:    Projects/Project0/target/surefire-reports/com.revature.Project0.services.UserServiceImplTest.txt

--- Projects/Project0/sql_ref.md
+++
@@ -1,9 +0,0 @@
-# Open terminal
-``pqsl psql -U postgres -d projectDB``
-# Enter password when prompted
-# In the psql shell
-TRUNCATE TABLE users CASCADE;
-# Or to truncate multiple tables
-``pqsl TRUNCATE TABLE users, items;``
-# To exit psql
-``pqsl \q``
--- Projects/Project0/src/main/java/com/revature/Project0/repositories/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.revature.Project0.repositories;
-
-import com.revature.Project0.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
