a0288fc7d73facc20da009bd9142ab6edda79d0c
Andrew T <me@andrewtran-shi.dev>
2024-08-20 09:03:49 -0700

    feat: Add KafkaConsumerService and KafkaProducerService to KafkaProducerController

--- Labs/Kafka/kafkaDemo/src/main/java/com/revature/demo/controllers/KafkaProducerController.java
+++ Labs/Kafka/kafkaDemo/src/main/java/com/revature/demo/controllers/KafkaProducerController.java
@@ -1,11 +1,15 @@
 package com.revature.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.demo.services.KafkaConsumerService;
 import com.revature.demo.services.KafkaProducerService;
 
 @RestController
@@ -15,12 +19,22 @@ public class KafkaProducerController {
     @Autowired
     private KafkaProducerService kafkaProducerService;
 
+    @Autowired
+    private KafkaConsumerService kafkaConsumerService;
+
     @PostMapping("/publish")
     public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
         // http://localhost:8080/kafka/publish?message=hello
 
-        // Let's print the message to the console for now
+        // Print the message to the console
         System.out.println("Message: " + message);
         kafkaProducerService.sendMessage(message);
+    }
+
+    @GetMapping("/messages")
+    public List<String> getMessages() {
+        // http://localhost:8080/kafka/messages
+        // Retrieve messages from Kafka
+        return kafkaConsumerService.getMessages();
     }
 }
--- Labs/Kafka/kafkaDemo/src/main/java/com/revature/demo/services/KafkaConsumerService.java
+++ Labs/Kafka/kafkaDemo/src/main/java/com/revature/demo/services/KafkaConsumerService.java
@@ -1,5 +1,8 @@
 package com.revature.demo.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,11 +13,17 @@ public class KafkaConsumerService {
 
     //Logger
     private final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
+    private final List<String> messages = new ArrayList<>();
 
     //Now we need a method that allows us to listen for the kafka message
     @KafkaListener(topics = "test")
     public void consume(String message) {
+        messages.add(message);
         logger.info("Message received ->" + message);
+    }
+
+    public List<String> getMessages() {
+        return new ArrayList<>(messages);
     }
 
 }
